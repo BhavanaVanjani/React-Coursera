4eb27b6c944264f07b47f7bc683aa86eab7e4824
BhavanaVanjani <bhavana.vanjani@gmail.com>
2019-08-19 15:22:39 +0530

    Assignment-3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,14 +1,103 @@
-import React from 'react';
-import {Card,CardImg,CardText,CardBody,CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import React, {Component} from 'react';
+import {Card,CardImg,CardText,CardBody,CardTitle,Breadcrumb,BreadcrumbItem,Button,Modal,ModalBody,ModalHeader,Row,Col,Label} from 'reactstrap';
+import {Control,LocalForm,Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
 
-// componentDidMount(){
-//   console.log('DishDetail Component componentDidMount invoked');
-// }
-//
-// componentDidUpdate(){
-//      console.log('DishDetail Component componentDidUpdate invoked');
-//   }
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len)
+const minLength = (len) => (val) =>  (val) && (val.length >= len)
+
+
+class CommentForm extends Component
+{
+  constructor(props){
+    super(props);
+    this.state = {
+      isModalOpen:false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+        isModalOpen:!(this.state.isModalOpen)
+    });
+  }
+
+  handleSubmit=(values)=>{
+    this.toggleModal();
+     console.log("Current State is :" + JSON.stringify(values));
+      alert("Current State is:" + JSON.stringify(values));
+  };
+
+  render() {
+    return(
+      <div className="container">
+      <div className="ml-0">
+      <Button outline onClick={this.toggleModal}>
+      <span class="fa fa-pencil fa-lg"></span> Submit Comment
+      </Button>
+      <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+      <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+      <ModalBody>
+      <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+      <Row className="form-group">
+      <Label htmlFor="rating" md={12}> Rating </Label>
+      <Col md={12}>
+      <Control.select model=".rating" className="form-control">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      </Control.select>
+      </Col>
+      </Row>
+      <Row className="form-group">
+      <Label htmlFor="author" md={12}> Your Name </Label>
+      <Col md={12}>
+      <Control.text model=".author" className="form-control" id="author" name="author"
+           placeholder="Your Name"  validators={{
+             required, minLength: minLength(3) ,maxLength: maxLength(15)
+           }} />
+           <Errors
+              className="text-danger" model=".author"
+              show="touched"
+              messages={{
+                required: ' Required ',
+                minLength: 'Must be greater than 2 characters',
+                maxLength: 'Must be 15 characters or less'
+              }} />
+      </Col>
+      </Row>
+      <Row className="form-group">
+      <Label htmlFor="comment" md={12}>Comment</Label>
+      <Col md={12}>
+      <Control.textarea model=".comment" id="comment" name="comment" rows="6" className="form-control"/>
+      </Col>
+      </Row>
+      <Row className="form-group">
+      <Col md={{size:12}}>
+      <Button type="submit" color="primary">Submit</Button>
+      </Col>
+      </Row>
+      </LocalForm>
+      </ ModalBody>
+      </Modal>
+      </div>
+      </div>
+// <Form onSubmit={this.handleSubmit}>
+      // <FormGroup>
+      // <Label htmlFor="username">Username</Label>
+      // <Input type="text" id="username" name="username"
+      //    innerRef={(input) => this.username = input} />
+      // </FormGroup>
+      // </Form>
+    );
+  }
+
+}
 
   function RenderDish({dish}) {
      if(dish!= null){
@@ -29,7 +118,7 @@ import {Link} from 'react-router-dom';
      }
     }
 
-  function RenderComments({comments}){
+  function RenderComments({comments,dishId}){
      if(comments != null){
       const commentsList = comments.map((comm) =>{
           return(
@@ -47,6 +136,7 @@ import {Link} from 'react-router-dom';
           <div>
           <h4> Comments : </h4>
           {commentsList}
+          <CommentForm dishId={dishId}/>
           </div>
         );
     }
