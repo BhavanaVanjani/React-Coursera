d6ecc461883b3702fbe7d21e5713d4ed00401669
BhavanaVanjani <bhavana.vanjani@gmail.com>
2019-08-22 00:22:16 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Card,CardImg,CardText,CardBody,CardTitle,Breadcrumb,BreadcrumbItem,Button,Modal,ModalBody,ModalHeader,Row,Col,Label} from 'reactstrap';
 import {Control,LocalForm,Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
+import {Loading} from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len)
@@ -144,10 +145,28 @@ class CommentForm extends Component
   }
 
     const DishDetail=(props)=> {
-       console.log('DishDetail Component render invoked');
+       if(props.isLoading) {
+         return(
+             <div className="container">
+                <div className="row">
+                   <Loading />
+                </div>
+             </div>
+         );
+       }
+       else if(props.errMess){
+        return(
+          <div className="container">
+             <div className="row">
+                <h4>{props.errMess}</h4>
+             </div>
+          </div>
+      );
+      }
+       //console.log('DishDetail Component render invoked');
       //const dish = props.selectedDish ;
       //console.log(props);
-      if(props.dish != null){
+       else if(props.dish != null){
         return (
           <div className="container">
           <div className="row">
